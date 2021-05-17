40aaedae941c632c775a567ab27863d0c5382c07
Mike Loranty <mloranty@colgate.edu>
2021-05-17 11:55:09 -0400

    updated code to specify sensor placements based on timestamp

--- densityMet_data_clean.R
+++ densityMet_data_clean.R
@@ -58,7 +58,6 @@ for(i in 2:length(f))
 
 # PROCESS DATA FROM SITE MDF1
 #------------------------------------------------------------------#
-# process data from mdf1
 f <- list.files(path = "csvRaw", pattern = "mdf1", full.names = T)
 
 # read header info and data separately
@@ -69,13 +68,13 @@ d <- read.csv(f[1], na.strings = "#N/A",skip = 3, header = F)
 ts <- strptime(d[,1], format = "%m/%d/%y %R")
 
 # create create a long data frame
-dat <- data.frame(cbind(year(ts),yday(ts),hour(ts), minute(ts),h[1,1]),h[1,2],h[2,2],NA,h[3,2],d[,2])
-names(dat) <- c("year", "jday", "hour","min","logger", "port", "sensor", "sensorZ","unit", "measurement")
+dat <- data.frame(ts,h[1,1],h[1,2],h[2,2],NA,h[3,2],d[,2])
+names(dat) <- c("timestamp","logger", "port", "sensor", "sensorZ","unit", "measurement")
 
 # loop through each column and add to the data frame
 for(i in 3:ncol(d))
 {
-  td <- data.frame(cbind(year(ts),yday(ts),hour(ts), minute(ts),h[1,1]),h[1,i],h[2,i],NA,h[3,i],d[,i])
+  td <- data.frame(ts,h[1,1],h[1,i],h[2,i],NA,h[3,i],d[,i])
   names(td) <- names(dat)
   dat <- rbind(dat,td)
 }
@@ -90,24 +89,31 @@ for(i in 2:length(f))
   # format time stamp info
   ts <- strptime(d[,1], format = "%m/%d/%y %R") 
   
-  
   for(j in 2:ncol(d))
   {
-    td <- data.frame(cbind(year(ts),yday(ts),hour(ts), minute(ts),h[1,1]),h[1,i],h[2,i],NA,h[3,i],d[,i])
+    td <- data.frame(ts,h[1,1],h[1,i],h[2,i],NA,h[3,i],d[,i])
     names(td) <- names(dat)
     dat <- rbind(dat,td) 
   }
 }
 
 # set sensorZ values (positive/negative for above/below ground)
+# Ports 1 & 2 are PAR and Airtemp/RH at 1m above ground
+# Port 3 is soil sensor at Organic/Mineral interface -6cm
+# Port 4 is soil sensor initially placed -11cm below surface but then moved to -16cm on date specified
 
 dat$sensorZ <- ifelse(dat$port == "Port 1"|dat$port == "Port 2", 1, 
-                      ifelse(dat$port == "Port 3", 6, 
-                             iflese(dat$port == "Port 4" & dat$year < 2016 & dat$jday < 247 & dat$hour, dat$sensorZ)) 
-                      # need to figure out a logical way to specify when sensorZ changed
-                      # maybe can use less then with date/time posix options? 
-
+                      ifelse(dat$port == "Port 3", -6, 
+                             ifelse(dat$port == "Port 4" & dat$timestamp < as.POSIXct("2015-09-03 16:30"), -11, -16))) 
 #------------------------------------------------------------------#
 
+# MDF2 ifelse(dat$port == "Port 4" & dat$timestamp < as.POSIXct("2015-09-03 13:00"), -14, -19)
+# LDF2 ifelse(dat$port == "Port 4" & dat$timestamp < as.POSIXct("2015-09-03 19:30"), -13, -18)
 
+# LBR ifelse(dat$port == "Port 4" & dat$timestamp < as.POSIXct("2015-09-03 17:00"), -23, -28) ## 
+#note here that ports 3 & 4 may were switched - the installation was poor as well,
+# so until this date Port 4 was the O/M sensor, but contact was poor and data were bad
 
+# HDF1 ifelse(dat$port == "Port 4" & dat$timestamp < as.POSIXct("2015-09-03 19:00"), -14, -19)
+# DAV ifelse(dat$port == "Port 4" & dat$timestamp < as.POSIXct("2015-09-04 17:00"), -15, -20)
+# 
